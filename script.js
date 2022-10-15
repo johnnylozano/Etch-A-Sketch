cbeb77459498011c84d2fc210f150d6b4541d8ea
johnnylozano <102393958+johnnylozano@users.noreply.github.com>
2022-10-15 18:59:52 -0400

    Finished grid, added styling

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-const div = document.createElement("div");
-const row = document.querySelector(".row");
 const sketchContainer = document.querySelector(".sketch-container");
+const button = document.querySelector(".btn");
 
-let grid = 16;
+let grid = 0;
+function setGridSize(grid) {
+   if (grid !== 0) {
+      clearGrid();
+   }
+   if (grid > 100) grid = 100;
 
-// for (let i = 0; i < grid; i++) {
-//    sketchContainer.insertBefore(document.createElement("div"), row);
-//    div.classList.add("row");
-//    row.appendChild(document.createElement("div"));
-//    row.lastChild.classList.add("square");
-// }
+   for (let j = 0; j < grid; j++) {
+      sketchContainer.appendChild(document.createElement("div"));
+      sketchContainer.lastChild.classList.add("row");
 
-sketchContainer.insertBefore(document.createElement("div"), row);
-let newRow = sketchContainer.firstChildElement;
-newRow.classList.add("row");
-// div.classList.add("square");
+      for (let i = 0; i < grid; i++) {
+         let lastRow = sketchContainer.lastChild;
+         lastRow.appendChild(document.createElement("div"));
+         lastRow.lastChild.classList.add("square");
+      }
+   }
+   return drawGrid();
+}
+
+button.addEventListener("click", () => {
+   let grid = prompt("Please enter a number up to 100");
+   parseInt(grid);
+   return setGridSize(grid);
+});
+
+function drawGrid() {
+   let square = document.querySelectorAll(".square");
+
+   square.forEach((sq) => {
+      sq.addEventListener("mouseover", () => {
+         sq.classList.add("dark");
+      });
+   });
+}
+
+function clearGrid() {
+   let row = document.querySelectorAll(".row");
+
+   row.forEach((r) => {
+      r.remove();
+   });
+}
